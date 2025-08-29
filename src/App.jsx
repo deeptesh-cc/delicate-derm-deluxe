a7f5a9ef5732bef13970f4b1be031baa584cad69
unknown <deeptesh.kumar@codeclouds.com>
2025-08-29 18:17:07 +0530

    thank you page design

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CheckoutPage from "./pages/CheckoutPage";
 import ProductDetails from "./pages/ProductDetails";
 import CartPage from "./pages/CartPage";
 import ContactUs from "./pages/ContactUs";
+import ThankYou from "./pages/ThankYou";
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
             <Route path="/cart" element={<CartPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
             <Route path="/contact" element={<ContactUs />} />
+            <Route path="/thank-you" element={<ThankYou />} />
           </Routes>
       <Footer/>
     </div>
---
+++ src/pages/ThankYou.jsx
@@ -0,0 +1,57 @@
+import SectionTitle from "../components/SectionTitle"
+import Button from "../components/Button"
+
+function ThankYou() {
+
+    const today = new Date();
+      const day = today.getDate();
+      const month = today.toLocaleString('default', { month: 'long' }); // getMonth() is 0-indexed
+      const year = today.getFullYear();
+
+    return(
+        <section className="sectionGap">
+            <div className="container">
+            <div className="row justify-content-center">
+            <div className="col-lg-8">
+                <div className="text-center">
+                    <img src="/images/icons/order-placed.svg" alt="" class="mb-4"/>
+                    <SectionTitle
+                    title="Thank You For Your Order"
+                    extraclasses="mb-4"
+                    />
+                    <p className="mb-4">We have accepted your order, and we're getting it ready. <br/> A confirmation mail has been sent to Your Mail ID.</p>
+                </div>
+                <div className="order-bill-area">
+                    <ul>
+                        <li className="">
+                            Order ID:
+                            <span>#2837484892</span>
+                        </li>
+                        <li className="">
+                            Order Date:
+                            <span>{month} {day}, {year}</span>
+                        </li>
+                        <li className="">
+                            Items:
+                            <span id="billedItems">2</span>
+                        </li>
+                        <li className="">
+                            Order Total:
+                            <span id="billedTotal">$15</span>
+                        </li>
+                    </ul>
+                </div>
+                <p class="text-center mb-4">
+                    <img src="/images/icons/shipping.svg" class="me-1" width="40" /> Estimated delivery between 3-5 business days
+                 </p>
+                 <div className="text-center">
+                 <Button to="/" variant="solid">Back To Home</Button>
+                 </div>
+            </div>
+            </div>
+            </div>
+        </section>
+    )
+}
+
+export default ThankYou
